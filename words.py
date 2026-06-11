530fa498866760153271913defe1ed3e9874c023
amirhoho94-lang <amirhoho94@gmail.com>
2026-06-11 07:20:05 +0330

    Create words.py

---
+++ words.py
@@ -0,0 +1,7 @@
+# لیست کلمات و معنی‌شون
+words = [
+    {"word": "apple", "meaning": "سیب", "level": "A1"},
+    {"word": "banana", "meaning": "موز", "level": "A1"},
+    {"word": "house", "meaning": "خانه", "level": "A2"},
+    # می‌تونی خودت کلمه اضافه کنی
+]
